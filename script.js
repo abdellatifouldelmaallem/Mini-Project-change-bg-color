62019082910b92796f560e2a13e3549265406f8a
abdellatif <ouldeabdellatif40@gmail.com>
2021-04-27 23:57:18 +0000

    add function it works while i click on button the b-ground changes the color with hex color

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 const colors = ["red","yellow","green","blue","pink","purple","gray","white"];
 const hexColor =["#ff0000","#ffff00","#008000","#0000ff","#ffc0cb","#800080","#808080","#ffffff"]
+
+const Btn = document.getElementById('click');
+
+
+
+var change = document.getElementById('body');
+var codeColor = document.getElementById('hexcolor');
+
+Btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const random = Math.floor(Math.random()*colors.length);
+    change.style.backgroundColor = colors[random];
+    codeColor.textContent = "Hex Color : "+hexColor[random].toString().toUpperCase()
+}
+)
